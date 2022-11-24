6d48afd93d7f705476c0617db133371e24bbd851
NSUSpray <nsu.spray@gmail.com>
2022-11-24 11:56:23 +0600

    Get exchange rate from exchangerate-api.com, add getting example

--- src/parser.py
+++ src/parser.py
@@ -1,4 +1,22 @@
+import os
+import requests
+
+
+API_KEY = os.environ['EXCHANGE_RATE_API_KEY']
+
+
 class Parser:
 
-	def get_exchange_rate(self, src, dst):
-		return 1.3  # TODO
+    def __init__(self):
+        self.base_url = f'https://v6.exchangerate-api.com/v6/{API_KEY}/pair'
+
+    def get_exchange_rate(self, src, dst):
+        url = f'{self.base_url}/{src.upper()}/{dst.upper()}'
+        data = requests.get(url).json()
+        return data['conversion_rate']
+
+
+if __name__ == '__main__':
+    parser = Parser()
+    rate = parser.get_exchange_rate('USD', 'MNT')
+    print(f'One dollar in tugriks: {rate}')
