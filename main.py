e12ca89b06a062484e327e69726bf8415ceee8d3
alnchk <anton.lonchakov@ya.ru>
2022-12-31 11:00:51 +0600

    changes from second meeting

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from telebot import types
 
 import valut4ik_bot.converter as converter
 from config import BOT_TOKEN
-from resources import *
+from resources import BUTTON_NAMES
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
-converter = converter.Converter()
+converter_obj = converter.Converter()
 
 
 @bot.message_handler(commands=['start'])
@@ -19,24 +19,24 @@ def start(message):
             one_time_keyboard=False
             )
     markup.add(
-            types.KeyboardButton(RUB_SOM_BTNTXT),
-            types.KeyboardButton(SOM_RUB_BTNTXT),
-            types.KeyboardButton(ASK_RATE_BTNTXT),
+            types.KeyboardButton(BUTTON_NAMES['RUB_SOM_BTNTXT']),
+            types.KeyboardButton(BUTTON_NAMES['SOM_RUB_BTNTXT']),
+            types.KeyboardButton(BUTTON_NAMES['ASK_RATE_BTNTXT']),
             )
     bot.send_message(message.chat.id, greet, reply_markup=markup)
 
 
 @bot.message_handler(content_types='text')
 def valuta(message):
-    if message.text == ASK_RATE_BTNTXT:
+    if message.text == BUTTON_NAMES['ASK_RATE_BTNTXT']:
         bot.send_message(
                 message.chat.id,
-                f'За 1 рубль нынче дают {converter.one_rub_in_kgs()} сом'
+                f'За 1 рубль нынче дают {converter_obj.one_rub_in_kgs()} сом'
                 )
-    elif message.text == RUB_SOM_BTNTXT:
+    elif message.text == BUTTON_NAMES['RUB_SOM_BTNTXT']:
         msg = bot.reply_to(message, 'Сколько?')
         bot.register_next_step_handler(msg, rub_to_som)
-    elif message.text == SOM_RUB_BTNTXT:
+    elif message.text == BUTTON_NAMES['SOM_RUB_BTNTXT']:
         msg = bot.reply_to(message, 'Сколько?')
         bot.register_next_step_handler(msg, som_to_rub)
 
@@ -44,18 +44,18 @@ def valuta(message):
 def rub_to_som(message):
     try:
         amount = float(message.text)
-        converted_amount = converter.rub_to_kgs(amount)
+        converted_amount = converter_obj.rub_to_kgs(amount)
         bot.reply_to(message, converted_amount)
-    except converter.ConversionError:
+    except (converter.ConversionError, ValueError):
         bot.reply_to(message, 'Чё-то не смог посчтитать...')
 
 
 def som_to_rub(message):
     try:
         amount = float(message.text)
-        converted_amount = converter.kgs_to_rub(amount)
+        converted_amount = converter_obj.kgs_to_rub(amount)
         bot.reply_to(message, converted_amount)
-    except converter.ConversionError:
+    except (converter.ConversionError, ValueError):
         bot.reply_to(message, 'Чё-то не смог посчтитать...')
 
 
